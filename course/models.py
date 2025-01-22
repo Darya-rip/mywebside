589963ff245abbf8619bf19f09c252780cc125d5
Dasha <gyseynova.d@gmail.com>
2025-01-22 15:28:31 +0500

    create class

--- course/models.py
+++ course/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
 # Create your models here.
+
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    keywords = models.CharField(max_length=255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.title
+
+class Subject(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    subject_tutor = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    price = models.CharField(max_length=100)
+    detail = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    def __str__(self):
+        return self.title
+
